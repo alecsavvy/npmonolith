2ed790947161ddbaafb415142c08fed06bb1be03
Alec Savvy <alecsavvy@outlook.com>
2023-08-03 22:28:13 -0600

    database skeleton

--- libs/config/src/index.ts
+++ libs/config/src/index.ts
@@ -1,11 +1,8 @@
-import dotenv from "dotenv"
-
 export type Config = {
     databaseUrl: string,
 }
 
 export const readConfig = (): Config => {
-    dotenv.config()
     const databaseUrl = process.env.databaseUrl
     if (databaseUrl === undefined) throw new Error("databaseUrl undefined")
     return { databaseUrl }
---
+++ libs/database/src/index.ts
@@ -0,0 +1,29 @@
+import { Knex, knex } from "knex"
+
+export const instantiateDb = (url: string): Knex => {
+    return knex({
+        client: "pg",
+        connection: url
+    })
+}
+
+export type Block = {
+    blockNumber: number,
+    blockHash: string,
+}
+
+export const migrations = async (db: Knex) => {
+    // TODO: create table if not exists
+}
+
+export const insertBlock = async (db: Knex): Promise<Block> => {
+    throw new Error("not implemented")
+}
+
+export const getBlock = async (db: Knex, blockNumber: number): Promise<Block> => {
+    throw new Error("not implemented")
+}
+
+export const getBlocks = async (db: Knex, count = 10): Promise<Block[]> => {
+    return []
+}
